4343c41833aa8d95ca7f4154bf0a71d7553b8395
Joel Turner <joel@joelmturner.com>
2025-01-13 22:54:52 -0800

    fixes entry name

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { resolve } from "path";
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/nav-bar.ts"),
+      entry: resolve(__dirname, "src/zine-viewer.ts"),
       name: "NavBar",
-      fileName: (format) => `nav-bar.${format}.js`,
+      fileName: format => `zine-viewer.${format}.js`,
       formats: ["es", "umd"],
     },
     rollupOptions: {
